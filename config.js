0554347763f4fe94460aa7fc45a802a779096ec9
demo-qr <111203627+demo-qr@users.noreply.github.com>
2024-03-03 12:39:34 -0300

    config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbznFhybxnk58dT-JDK8oreStZxp9vVyFMta0wUiZWkmoV7YS47vivQ1EB9tFGYFMnMvBg/exec";
