5f4cce3c473fad800a83ba7fdf43ee46bfd5d511
demo-qr <111203627+demo-qr@users.noreply.github.com>
2024-03-03 15:58:10 -0300

    config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbznFhybxnk58dT-JDK8oreStZxp9vVyFMta0wUiZWkmoV7YS47vivQ1EB9tFGYFMnMvBg/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbx4jMyZ9kfZkHDMjljjNRGThSG6sEORjVx4h3eEsKZQLWHjxrRAE4FpgYgVC9E3Nbi-Nw/exec";
